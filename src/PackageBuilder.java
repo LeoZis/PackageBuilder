9d9ecb3b6a92c2d2b07819ded51c0e2e52127cfb
Justin Catterson <justin.catterson@gmail.com>
2014-08-14 23:07:36 -0400

    When a string does not have a dotIndex[-1] the code would crash when substringing

--- src/PackageBuilder.java
+++ src/PackageBuilder.java
@@ -21,7 +21,7 @@ import org.w3c.dom.Text;
 
 public class PackageBuilder
  {
-	private static final Map<String, String> extToPackageName;
+    private static final Map<String, String> extToPackageName;
 
     static
     {
@@ -34,183 +34,185 @@ public class PackageBuilder
         extToPackageName = Collections.unmodifiableMap(tmpMap);
     }
 
-	private void createXML(Map<String, ArrayList<String>> fileMap, StreamResult result)
-	{
+    private void createXML(Map<String, ArrayList<String>> fileMap, StreamResult result)
+    {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder;
-		try {
-			docBuilder = docFactory.newDocumentBuilder();
+        DocumentBuilder docBuilder;
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
 
-			Document doc = docBuilder.newDocument();
-			Element root = doc.createElement("Package");
-			doc.appendChild(root);
+            Document doc = docBuilder.newDocument();
+            Element root = doc.createElement("Package");
+            doc.appendChild(root);
 
-	        for(String key : fileMap.keySet())
-	        {
-	        	Element types = doc.createElement("types");
-	        	root.appendChild(types);
+            for(String key : fileMap.keySet())
+            {
+                Element types = doc.createElement("types");
+                root.appendChild(types);
 
-	        	for(String name : fileMap.get(key))
-	        	{
-	        		Element member = doc.createElement("members");
-	        		member.appendChild(doc.createTextNode(name));
-	        		types.appendChild(member);
-	        	}
+                for(String name : fileMap.get(key))
+                {
+                    Element member = doc.createElement("members");
+                    member.appendChild(doc.createTextNode(name));
+                    types.appendChild(member);
+                }
 
-	        	Element name = doc.createElement("name");
-	            name.appendChild(doc.createTextNode(this.getPackageName(key)));
-	        	types.appendChild(name);
-	        }
+                Element name = doc.createElement("name");
+                name.appendChild(doc.createTextNode(this.getPackageName(key)));
+                types.appendChild(name);
+            }
 
-	        Element version = doc.createElement("version");
-	        version.appendChild(doc.createTextNode("29.0"));
-	        root.appendChild(version);
+            Element version = doc.createElement("version");
+            version.appendChild(doc.createTextNode("29.0"));
+            root.appendChild(version);
 
-	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer;
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer;
 
-			transformer = transformerFactory.newTransformer();
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-			DOMSource source = new DOMSource(doc);
-			transformer.transform(source, result);
-		}
-		catch (ParserConfigurationException e)
-		{
-			e.printStackTrace();
-		}
-		catch (TransformerConfigurationException e)
-		{
-			e.printStackTrace();
-		}
-		catch (TransformerException e)
-		{
-			e.printStackTrace();
-		}
-	}
+            transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            DOMSource source = new DOMSource(doc);
+            transformer.transform(source, result);
+        }
+        catch (ParserConfigurationException e)
+        {
+            e.printStackTrace();
+        }
+        catch (TransformerConfigurationException e)
+        {
+            e.printStackTrace();
+        }
+        catch (TransformerException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-	private void createXML(Map<String, ArrayList<String>> fileMap, File file)
-	{
-		this.createXML(fileMap, new StreamResult(file));
-	}
+    private void createXML(Map<String, ArrayList<String>> fileMap, File file)
+    {
+        this.createXML(fileMap, new StreamResult(file));
+    }
 
-	private void createXML(Map<String, ArrayList<String>> fileMap, OutputStream stream)
-	{
+    private void createXML(Map<String, ArrayList<String>> fileMap, OutputStream stream)
+    {
 
-		this.createXML(fileMap, new StreamResult(stream));
-	}
+        this.createXML(fileMap, new StreamResult(stream));
+    }
 
-	private void createXML(Map<String, ArrayList<String>> fileMap)
-	{
-		this.createXML(fileMap, new File("package.xml"));
-	}
+    private void createXML(Map<String, ArrayList<String>> fileMap)
+    {
+        this.createXML(fileMap, new File("package.xml"));
+    }
 
-	private String getPackageName(String name)
-	{
-		if(extToPackageName.containsKey(name))
-		{
-			return extToPackageName.get(name);
-		}
-		return "";
-	}
+    private String getPackageName(String name)
+    {
+        if(extToPackageName.containsKey(name))
+        {
+            return extToPackageName.get(name);
+        }
+        return "";
+    }
 
-	private String getFileContents(String fileName)
-	{
-		BufferedReader br;
-		String contents = null;
+    private String getFileContents(String fileName)
+    {
+        BufferedReader br;
+        String contents = null;
 
-		if(fileName == null)
-			return null;
+        if(fileName == null)
+            return null;
 
-		try
-		{
-	    	br = new BufferedReader(new FileReader(fileName));
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
+        try
+        {
+            br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-	        while (line != null)
-	        {
-	            sb.append(line);
-	            sb.append('\n');
-	            line = br.readLine();
-	        }
+            while (line != null)
+            {
+                sb.append(line);
+                sb.append('\n');
+                line = br.readLine();
+            }
 
-	        br.close();
+            br.close();
 
-	        contents = sb.toString();
-		}
-		catch (IOException e)
-	    {
-			System.out.println("Error reading file");
-			e.printStackTrace();
-		}
-	   return contents;
-	}
+            contents = sb.toString();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error reading file");
+            e.printStackTrace();
+        }
+       return contents;
+    }
 
-	private Map<String, ArrayList<String>> getMapFromContents(String contents)
-	{
-		String[] packageArr = contents.split("\n");
+    private Map<String, ArrayList<String>> getMapFromContents(String contents)
+    {
+        String[] packageArr = contents.split("\n");
         Map<String, ArrayList<String>> extToFileName = new HashMap<String, ArrayList<String>>();
 
         for(String str : packageArr)
         {
-        	int slashIndex = str.lastIndexOf("/");
-        	int dotIndex = str.lastIndexOf(".");
+            int slashIndex = str.lastIndexOf("/");
+            int dotIndex = str.lastIndexOf(".");
+            
+            if( dotIndex == -1 )
+                continue;
+            String fileName = str.substring(slashIndex+1, dotIndex);
+            String extName = str.substring(dotIndex+1, str.length());
 
-        	String fileName = str.substring(slashIndex+1, dotIndex);
-        	String extName = str.substring(dotIndex+1, str.length());
-
-        	if(!extToFileName.containsKey(extName) && extToPackageName.containsKey(extName))
-        	{
-        		extToFileName.put(extName, new ArrayList<String>());
-        	}
-        	if(extToPackageName.containsKey(extName))
-        		extToFileName.get(extName).add(fileName);
+            if(!extToFileName.containsKey(extName) && extToPackageName.containsKey(extName))
+            {
+                extToFileName.put(extName, new ArrayList<String>());
+            }
+            if(extToPackageName.containsKey(extName))
+                extToFileName.get(extName).add(fileName);
         }
 
         return extToFileName;
-	}
+    }
 
-	public static void main(String[] args)
-	{
-		String fromFile = null;
-		String toFile = null;
-		if(args != null)
-		{
-			if(args.length == 1)
-			{
-				fromFile = args[0];
-			}
-			else if(args.length == 2)
-			{
-				fromFile = args[0];
-				toFile = args[1];
-			}
-		}
+    public static void main(String[] args)
+    {
+        String fromFile = null;
+        String toFile = null;
+        if(args != null)
+        {
+            if(args.length == 1)
+            {
+                fromFile = args[0];
+            }
+            else if(args.length == 2)
+            {
+                fromFile = args[0];
+                toFile = args[1];
+            }
+        }
 
-		PackageBuilder pb = new PackageBuilder();
+        PackageBuilder pb = new PackageBuilder();
 
-	    String contents = pb.getFileContents(fromFile);
+        String contents = pb.getFileContents(fromFile);
 
-	    if(contents == null)
-	    {
-	    	System.out.println("Error: Must supply valid file name as argument");
-	    	return;
-	    }
+        if(contents == null)
+        {
+            System.out.println("Error: Must supply valid file name as argument");
+            return;
+        }
 
-	    Map<String, ArrayList<String>> extToFileName = pb.getMapFromContents(contents);
+        Map<String, ArrayList<String>> extToFileName = pb.getMapFromContents(contents);
 
         if(toFile == null)
         {
-        	pb.createXML(extToFileName);
+            pb.createXML(extToFileName);
         }
         else if(toFile.equals("console"))
         {
-        	pb.createXML(extToFileName, System.out);
+            pb.createXML(extToFileName, System.out);
         }
         else
         {
-        	pb.createXML(extToFileName, new File(args[1]));
+            pb.createXML(extToFileName, new File(args[1]));
         }
     }
 }
